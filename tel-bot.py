eedc27544a8d690cdb5150bf67222b782625e421
davendan <dap09310@gmail.com>
2016-09-17 18:19:38 +0200

    add_torrent Ask and Response added

--- tel-bot.py
+++ tel-bot.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 from urllib2 import urlopen
 import transmissionrpc
 import datetime
 
+# Load configuration file
 f=open('config.txt', 'r')
 TOKEN=f.readline()[:-1]
 USER_ID=int(f.readline()[:-1])
@@ -18,6 +19,7 @@ dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
+waiting_torrent = False
 tc = transmissionrpc.Client(TORRENT[0], port=int(TORRENT[1]), user=TORRENT[2], password=TORRENT[3])
 
 def get_out(bot, update):
@@ -43,21 +45,40 @@ def ip(bot, update):
 
 def add_torrent(bot, update, args):
   if update.message.from_user.id == USER_ID:
-    link=args
-    torrent=tc.add_torrent(link[0])
-    #bot.sendMessage(chat_id=update.message.chat_id, text=torrent)
+    if (len(args) > 0):
+      link=args
+      torrent=tc.add_torrent(link[0])
+      #bot.sendMessage(chat_id=update.message.chat_id, text=torrent)
+    else:
+      bot.sendMessage(chat_id=update.message.chat_id, text="Send me the magnet link:")
+      global waiting_torrent
+      waiting_torrent = True
   else:
     get_out(bot, update)
   
+def nch(bot, update):
+  if update.message.from_user.id == USER_ID:
+    if waiting_torrent:
+      torrent=tc.add_torrent(update.message.text)
+      global waiting_torrent
+      waiting_torrent = False
+    else:
+      bot.sendMessage(chat_id=update.message.chat_id, text="Sorry, didn't understood")
+      
 
-start_handler=CommandHandler('start', start)
-ip_handler=CommandHandler('ip', ip)
-add_torrent_handler=CommandHandler('add_torrent', add_torrent, pass_args=True)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(ip_handler)
-dispatcher.add_handler(add_torrent_handler)
+def main():
+  start_handler=CommandHandler('start', start)
+  ip_handler=CommandHandler('ip', ip)
+  add_torrent_handler=CommandHandler('add_torrent', add_torrent, pass_args=True)
+  nch_handler=MessageHandler([Filters.text], nch)
+  dispatcher.add_handler(start_handler)
+  dispatcher.add_handler(ip_handler)
+  dispatcher.add_handler(add_torrent_handler)
+  dispatcher.add_handler(nch_handler)
+  
+  updater.start_polling()
+    
+  updater.idle()
 
-updater.start_polling()
-
-updater.idle()
-
+if __name__ == '__main__':
+  main()
